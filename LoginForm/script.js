45ba948b30cf7b8546f8327c33e81c731ebe4d9c
ElenaGQA <elena.grebeniuk@gmail.com>
2025-05-02 13:17:34 -0400

    update

---
+++ LoginForm/script.js
@@ -0,0 +1,6 @@
+const feedbackForm = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const ratingSelect = document.querySelector('#rating');
+const commentsTextarea = document.getElementById('#comments');
+const submitButton = feedbackForm.querySelector('#submitBtn');
